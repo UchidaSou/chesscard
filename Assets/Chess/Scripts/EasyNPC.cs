6902c669584819bccd7433c46a3c9f418bcdf74a
UchidaSou <20215119@g.aut.ac.jp>
2024-01-01 19:29:03 +0900

    説明動画追加・修正

--- Assets/Chess/Scripts/EasyNPC.cs
+++ Assets/Chess/Scripts/EasyNPC.cs
@@ -54,8 +54,8 @@ public class EasyNPC : Player
         if(PlayerPrefs.GetInt("Mode",0) == 1){
             return;
         }
-        //int x = UnityEngine.Random.Range(0,20);
-        int x = 10;
+        int x = UnityEngine.Random.Range(0,20);
+        //int x = 10;
         switch(x){
             case 0:
              GameObject.Find("Game").GetComponent<Game>().Resurrection();
